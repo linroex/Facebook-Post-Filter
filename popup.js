33d740222f42ed937b58920f40d6aa8f68ba87ea
linroex <linroex@coder.tw>
2013-10-03 14:08:43 +0800

    add switcher and UI Design

--- popup.js
+++ popup.js
@@ -1,14 +1,37 @@
 $(document).ready(function(){
     chrome.storage.sync.get('block.keyword',function(r){
         $('textarea').html(r['block.keyword']);
-    })
-    
+    });
+    chrome.storage.sync.get('switcher',function(e){
+        
+        if(e['switcher']=='on'){
+            $('#switcher').html('停用');
+            
+        }else if(e['switcher']=='off'){
+            $('#switcher').html('啟用');
+            
+        }
+    });
     $('#submit').click(function(){
-        chrome.storage.sync.set({'block.keyword':$('textarea').val()});
-        chrome.storage.sync.get('block.keyword',function(r){
-            $('textarea').html(r['block.keyword']);
-        })
+        chrome.storage.sync.set({'block.keyword':$('textarea').val()},function(){
+            chrome.storage.sync.get('block.keyword',function(r){
+                $('textarea').html(r['block.keyword']);
+            })    
+        });
+        
             
     })
-
+    $('#switcher').click(function(){
+        chrome.storage.sync.get('switcher',function(e){
+            
+            if(e['switcher']=='on'){
+                $('#switcher').html('啟用');
+                chrome.storage.sync.set({'switcher':'off'});
+            }else if(e['switcher']=='off'){
+                $('#switcher').html('停用');
+                chrome.storage.sync.set({'switcher':'on'});
+            }
+        });
+        
+    });
 })
--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
-    console.log('Load Facebook Post Filter');
-    filter();
-    window.setInterval('filter()',3000);    
+    console.log('Load Facebook Post Filter');    
+    
+    chrome.storage.sync.get('switcher',function(e){
+        if(e['switcher']=='on'){
+            filter();
+            window.setInterval('filter()',3000);        
+        }else if(e['switcher']==undefined){
+            chrome.storage.sync.set({'switcher':'on'});
+            chrome.storage.sync.set({'block.keyword':''});
+        }
+        
+    });
+    
+    
 })
 function filter(){
     chrome.storage.sync.get('block.keyword',function(r){
         if(r['block.keyword'].trim()!=''){
             var keyword = r['block.keyword'].split("\n");
-            
             for (var i = 0; i<keyword.length; i++) {
-                
                 $('._5jmm:contains("' + keyword[i] + '")').fadeOut();
                 $('.storyContent:contains("' + keyword[i] + '")').fadeOut();
                 
